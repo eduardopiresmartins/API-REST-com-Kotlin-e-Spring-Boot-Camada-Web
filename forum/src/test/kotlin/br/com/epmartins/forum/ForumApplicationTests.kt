42f57881ab899cdbe32d8c4615725e1b5ee2ddcc
Eduardo Pires Martins <edupiresmartins@gmail.com>
2022-02-14 00:20:09 -0300

    inicio
    inicio do projeto

---
+++ forum/src/test/kotlin/br/com/epmartins/forum/ForumApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.epmartins.forum
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
